35d956dc0f597bcdcf1ad75123436ea12ce82a81
Kaiwen Wu <kps6326@hotmail.com>
2018-10-06 01:55:31 -0700

    Fixed the bug where the formula file passed in as the first argument is modified in place. This is regarded as a bug since user input shouldn't be altered. Also added support to read formula from stdin.

--- math2img.sh
+++ math2img.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 show_help() {
 	cat << EOF
-Usage: $0 output-image [ input-math-file ]
+Usage: $0 output-image [ input-math-file | - ]
 If \`input-math-file\` is not specified, it will be prompted in \$EDITOR,
-which if not set, default to "vi"). Input from stdin is not currently accepted.
+which if not set, default to "vi"). If \`input-math-file\` is specified as '-',
+The formula will be read from stdin.
 
 Usage example:
 	echo 'f(x)=2x+1' > math.txt
@@ -19,52 +20,80 @@ Return code:
 EOF
 }
 
-# prepare arguments
-srcdir=$(dirname ${BASH_SOURCE[0]})
-if [ -z "$1" ]; then
-	show_help
-	exit 1
-elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-	show_help
-	exit 0
-fi
-outfile=$(realpath "$1")
-if [ -z "$2" ]; then
-	rm -f "$srcdir/math.tmp" "$srcdir"/*.swp
-	touch "$srcdir/math.tmp"
-	if [ -z "$EDITOR" ]; then
-		EDITOR=vi
-	fi
-	$EDITOR "$srcdir/math.tmp"
-	mathfile="$srcdir/math.tmp"
-else
-	mathfile="$2"
-fi
-if ! "$srcdir/hasformula.py" "$mathfile"; then
-	echo "Math expression is empty; aborted"
-	exit 0
-fi
-"$srcdir/stripformula.py" "$mathfile"
 
-# conversion
-CURDIR=$(pwd)
-wdir=$(mktemp -d)
-if [ "$CURDIR" = "$wdir" ]; then
-	echo "Error creating temp directory; aborted"
-	exit 2
-fi
-cat "$srcdir/prefix.txt" "$mathfile" "$srcdir/suffix.txt" > "$wdir/math.tex"
-cd "$wdir"
-if ! pdflatex math.tex; then
+readonly srcdir=$(dirname ${BASH_SOURCE[0]})
+readonly localmathfile="$srcdir/math.tmp"
+
+
+parse_args() {
+	if [ -z "$1" ]; then
+		show_help
+		exit 1
+	elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		show_help
+		exit 0
+	fi
+	outfile="$(realpath "$1")"
+	mathfile="$2"
+}
+
+
+# Prepare math formula into $localmathfile
+prepare_math() {
+	rm -f "$srcdir"/*.swp
+	if [ -z "$mathfile" ]; then
+		if [ -z "$EDITOR" ]; then
+			EDITOR=vi
+		fi
+		> "$localmathfile"
+		$EDITOR "$localmathfile"
+	elif [ "$mathfile" = "-" ]; then
+		cat /dev/stdin > "$localmathfile"
+	elif [ -f "$mathfile" ]; then
+		cp "$mathfile" "$localmathfile"
+	else
+		# this leads to "Math expression is empty"
+		> "$localmathfile"
+	fi
+	if ! "$srcdir/hasformula.py" "$localmathfile"; then
+		echo "Math expression is empty; aborted"
+		exit 0
+	fi
+	"$srcdir/stripformula.py" "$localmathfile"
+}
+
+
+# Do the conversion $localmathfile -> LaTeX -> PDF -> Image
+conversion() {
+	local CURDIR=$(pwd)
+	local wdir=$(mktemp -d)
+	if [ "$CURDIR" = "$wdir" ]; then
+		echo "Error creating temp directory; aborted"
+		exit 2
+	fi
+	cat "$srcdir/prefix.txt" "$localmathfile" "$srcdir/suffix.txt" > "$wdir/math.tex"
+	cd "$wdir"
+	if ! pdflatex math.tex; then
+		cd "$CURDIR"
+		rm -rf "$wdir"
+		exit 3
+	fi
+	if ! convert -density 300 math.pdf -quality 90 -trim "$outfile"; then
+		cd "$CURDIR"
+		rm -rf "$wdir"
+		exit 4
+	fi
 	cd "$CURDIR"
 	rm -rf "$wdir"
-	exit 3
-fi
-if ! convert -density 300 math.pdf -quality 90 -trim "$outfile"; then
-	cd "$CURDIR"
-	rm -rf "$wdir"
-	exit 4
-fi
-cd "$CURDIR"
-rm -rf "$wdir"
-echo "Removed temp working directory $wdir"
+	echo "Removed temp working directory $wdir"
+}
+
+
+main() {
+	parse_args "$@"
+	prepare_math
+	conversion
+}
+
+
+main "$@"
