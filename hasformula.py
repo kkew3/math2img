8121a48d458d9560c6238f60c35f82b466b5b053
Kaiwen Wu <kps6326@hotmail.com>
2018-11-10 14:03:30 -0800

    Bug fix and use sed rather than python.
    Bug fixed: make the option combination '' invalid. This means that the
    option '-f' must be specified when '-o' is not.

--- hasformula.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-if __name__ == '__main__':
-    import sys
-    with open(sys.argv[1]) as infile:
-        content = infile.read()
-    content = content.strip()
-    if not content:
-        sys.exit(1)
